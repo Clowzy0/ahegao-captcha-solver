c22dc75870008f3d775768617f98f3b309693aa0
Clowzy <107867511+Clowzy0@users.noreply.github.com>
2022-06-20 16:52:03 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,173 @@
+#first of all: huge thanks to Chill from the discord server "Hello World!"
+#he helped me a lot with programming this
+
+#selenium imports (for browser interaction)
+import os
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+#PIL Import (for croping the entire iframe to the single faces)
+from PIL import Image
+
+#time import (just for delays)
+import time
+
+captchaURL = "https://captcha.ahegao.li"    #URL for the captcha (this Code only works for the Ahegao Captcha)
+waitTime = 2                                #general purpose delay time
+eaTime = 0.2                                #error avoiding time
+loadTime = 2                                #how long the site has to load fully
+                                            # (you might have to change this if you have slow internet)
+driver = webdriver.Firefox()                #selection of the browser for the geckodriver (geckodriver.exe)
+
+action_chains = ActionChains(driver)        #I actually don't know what these two do, but they're needed for clicking
+action = webdriver.common.action_chains.ActionChains(driver)
+
+driver.get(captchaURL)                                          #open the Website of the captcha
+
+time.sleep(loadTime)                                            # delay to avoid "element not found" error
+box_iframe = driver.find_element(By.ID, value="captcha-wrapper")#get the iframe where the box is positioned
+
+action.move_to_element_with_offset(box_iframe, 20, 38)          #move the "cursor" above the box...
+action.click()                                                  #and click it
+action.perform()                                                #this is needed for the click to work
+
+time.sleep(waitTime)                                            #wait for the faces to fully show
+
+element = driver.find_elements(By.TAG_NAME, value="iframe")     #select the iframe containing the faces
+element[0].screenshot('ahegao_uncroped.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')                 #select the screenshot to be croped
+xy_crop_values_0 = (1, 80, 80, 160)                             #x/y values for croping face 0
+face_0 = all_faces.crop(xy_crop_values_0)                       #crop face 0
+face_0.save('face_0.png')                                       #save the image for tensorflow
+                                                                #everything until time.sleep(60) is just croping
+all_faces = Image.open('./ahegao_uncroped.png')                 #all remaining faces, so I will not comment it
+xy_crop_values_1 = (81, 81, 160, 160)
+face_1 = all_faces.crop(xy_crop_values_1)
+face_1.save('face_1.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')
+xy_crop_values_2 = (161, 81, 240, 160)
+face_2 = all_faces.crop(xy_crop_values_2)
+face_2.save('face_2.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')
+xy_crop_values_3 = (1, 161, 80, 240)
+face_3 = all_faces.crop(xy_crop_values_3)
+face_3.save('face_3.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')
+xy_crop_values_4 = (81, 161, 160, 240)
+face_4 = all_faces.crop(xy_crop_values_4)
+face_4.save('face_4.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')
+xy_crop_values_5 = (161, 161, 240, 240)
+face_5 = all_faces.crop(xy_crop_values_5)
+face_5.save('face_5.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')
+xy_crop_values_6 = (1, 241, 80, 320)
+face_6 = all_faces.crop(xy_crop_values_6)
+face_6.save('face_6.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')
+xy_crop_values_7 = (81, 241, 160, 320)
+face_7 = all_faces.crop(xy_crop_values_7)
+face_7.save('face_7.png')
+
+all_faces = Image.open('./ahegao_uncroped.png')
+xy_crop_values_8 = (161, 241, 241, 320)
+face_8 = all_faces.crop(xy_crop_values_8)
+face_8.save('face_8.png')
+
+os.system("del ahegao_uncroped.png")                         #remove the uncroped image because it's not needed anymore
+os.system("python tensegao.py")                              #start tensorflow(tensegao.py)
+
+time.sleep(1)                                                #wait for tensorflow(tensegao.py) to completly end
+
+element = driver.find_elements(By.TAG_NAME, value="iframe")
+
+txt_file = open("result.txt", "r")                            #open the file which tensorflow saved the results in
+file_content = txt_file.read()                                #read the results
+result_list = file_content.split(",")                         #put the results into a python list
+txt_file.close()                                              #close the file
+
+box_iframe = driver.find_element(By.ID, value="captcha-wrapper")#select the iframe where the box is positioned
+
+action.move_to_element_with_offset(box_iframe, 20, 38)          #move the "cursor" above the box...
+action.click()                                                  #and click it
+action.perform()                                                #this is needed for the click to work
+
+if result_list[0] == "1":                                       #check if the face came back as ahegao-positive if yes:
+    action.move_to_element_with_offset(element[0], 44, 120)     #move the "cursor" above it
+    action.click()                                              #and
+    action.perform()                                            #click it
+
+time.sleep(eaTime)                                              #delay to avoid errors
+
+                                                                #all of this repeats until time.sleep(0.1)
+if result_list[1] == "1":                                       #with all other faces so I will not comment it
+    action.move_to_element_with_offset(element[0], 115, 120)
+    action.click()
+    action.perform()
+
+time.sleep(eaTime)
+
+if result_list[2] == "1":
+    action.move_to_element_with_offset(element[0], 218, 120)
+    action.click()
+    action.perform()
+
+time.sleep(eaTime)
+
+if result_list[3] == "1":
+    action.move_to_element_with_offset(element[0], 40, 204)
+    action.click()
+    action.perform()
+
+time.sleep(eaTime)
+
+if result_list[4] == "1":
+    action.move_to_element_with_offset(element[0], 118, 204)
+    action.click()
+    action.perform()
+
+
+time.sleep(eaTime)
+
+if result_list[5] == "1":
+    action.move_to_element_with_offset(element[0], 195, 204)
+    action.click()
+    action.perform()
+
+time.sleep(eaTime)
+
+if result_list[6] == "1":
+    action.move_to_element_with_offset(element[0], 44, 280)
+    action.click()
+    action.perform()
+
+time.sleep(eaTime)
+
+if result_list[7] == "1":
+    action.move_to_element_with_offset(element[0], 126, 280)
+    action.click()
+    action.perform()
+
+
+time.sleep(eaTime)
+
+if result_list[8] == "1":
+    action.move_to_element_with_offset(element[0], 200, 280)
+    action.click()
+    action.perform()
+
+
+time.sleep(0.1)                                                   #avoid errors
+
+action.move_to_element_with_offset(element[0], 205, 340)          #move the "cursor" above the box...
+action.click()                                                    #and
+action.perform()                                                  #click it
---
+++ tensegao.py
@@ -0,0 +1,250 @@
+import time
+
+import tensorflow.keras
+import numpy as np
+import cv2
+
+np.set_printoptions(suppress=False)
+
+model = tensorflow.keras.models.load_model('mod.h5')
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+result = ["0", "0", "0", "0", "0", "0", "0", "0", "0"]
+
+
+def ahe0():
+    imga = cv2.imread('face_0.png')
+    imga = cv2.resize(imga, (224, 224))
+
+    image_array = np.asarray(imga)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for a in prediction:
+        print(a)
+        if a[0] > a[1]:
+            result[0] = 1
+        if a[0] < a[1]:
+            result[0] = 0
+
+
+def ahe1():
+    imgb = cv2.imread('face_1.png')
+    imgb = cv2.resize(imgb, (224, 224))
+
+    image_array = np.asarray(imgb)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for b in prediction:
+        print(b)
+        if b[0] > b[1]:
+            result[1] = 1
+        if b[0] < b[1]:
+            result[1] = 0
+
+
+def ahe2():
+    imgc = cv2.imread('face_2.png')
+    imgc = cv2.resize(imgc, (224, 224))
+
+    image_array = np.asarray(imgc)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for c in prediction:
+        print(c)
+        if c[0] > c[1]:
+            result[2] = 1
+        if c[0] < c[1]:
+            result[2] = 0
+
+
+def ahe3():
+    imgd = cv2.imread('face_3.png')
+    imgd = cv2.resize(imgd, (224, 224))
+
+    image_array = np.asarray(imgd)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for d in prediction:
+        print(d)
+        if d[0] > d[1]:
+            result[3] = 1
+        if d[0] < d[1]:
+            result[3] = 0
+
+
+def ahe4():
+    imge = cv2.imread('face_4.png')
+    imge = cv2.resize(imge, (224, 224))
+
+    image_array = np.asarray(imge)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for e in prediction:
+        print(e)
+        if e[0] > e[1]:
+            result[4] = 1
+        if e[0] < e[1]:
+            result[4] = 0
+
+
+def ahe5():
+    imgf = cv2.imread('face_5.png')
+    imgf = cv2.resize(imgf, (224, 224))
+
+    image_array = np.asarray(imgf)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for f in prediction:
+        print(f)
+        if f[0] > f[1]:
+            result[5] = 1
+        if f[0] < f[1]:
+            result[5] = 0
+
+
+def ahe6():
+    imgg = cv2.imread('face_6.png')
+    imgg = cv2.resize(imgg, (224, 224))
+
+    image_array = np.asarray(imgg)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for g in prediction:
+        print(g)
+        if g[0] > g[1]:
+            result[6] = 1
+        if g[0] < g[1]:
+            result[6] = 0
+
+
+def ahe7():
+    imgh = cv2.imread('face_7.png')
+    imgh = cv2.resize(imgh, (224, 224))
+
+    image_array = np.asarray(imgh)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for h in prediction:
+        print(h)
+        if h[0] > h[1]:
+            result[7] = 1
+        if h[0] < h[1]:
+            result[7] = 0
+
+
+def ahe8():
+    imgi = cv2.imread('face_8.png')
+    imgi = cv2.resize(imgi, (224, 224))
+
+    image_array = np.asarray(imgi)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for i in prediction:
+        print(i)
+        if i[0] > i[1]:
+            result[8] = 1
+        if i[0] < i[1]:
+            result[8] = 0
+
+
+ahe0()
+ahe1()
+ahe2()
+ahe3()
+ahe4()
+ahe5()
+ahe6()
+ahe7()
+ahe8()
+
+print(result)
+
+with open('result.txt', 'w') as result_file:
+    if result[0] == 0:
+        result_file.writelines("0,")
+    if result[0] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[1] == 0:
+        result_file.writelines("0,")
+    if result[1] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[2] == 0:
+        result_file.writelines("0,")
+    if result[2] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[3] == 0:
+        result_file.writelines("0,")
+    if result[3] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[4] == 0:
+        result_file.writelines("0,")
+    if result[4] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[5] == 0:
+        result_file.writelines("0,")
+    if result[5] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[6] == 0:
+        result_file.writelines("0,")
+    if result[6] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[7] == 0:
+        result_file.writelines("0,")
+    if result[7] == 1:
+        result_file.writelines("1,")
+
+with open('result.txt', 'a') as result_file:
+    if result[8] == 0:
+        result_file.writelines("0")
+    if result[8] == 1:
+        result_file.writelines("1")
